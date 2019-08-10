6f91a194bc5cfab131f299f5dbe35a7f1ac48ddd
Celeo <celeodor@gmail.com>
2019-08-10 14:56:33 -0700

    About view

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+const aboutText = '''
+All references to Shadowrun game names or mechanics are property of Catalyst Game Labs.
+
+The Topps Company, Inc., (2019) Shadowrun, Sixth World. Spokane, WA: Catalyst Game Labs, an imprint of InMediaRes Productions, LLC.
+
+This app is not a substitute for purchasing the core rule book; you'll need to buy it in order to play the game.
+''';
+
 class ViewAbout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,14 +17,10 @@ class ViewAbout extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: Container(
-        padding: EdgeInsets.all(40.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'TODO',
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ],
+        padding: EdgeInsets.all(50.0),
+        child: Text(
+          aboutText,
+          style: TextStyle(fontSize: 20.0),
         ),
       ),
     );
